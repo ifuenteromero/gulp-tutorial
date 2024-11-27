38e746c8ba4c965fb043368c82b36d95111eb31a
Irene Fuente Romero <ifuenteromero@gmail.com>
2024-11-27 19:21:10 +0100

    Gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,21 @@
-function defaultTask(cb) {
+import { parallel, series } from 'gulp';
+
+function printName(cb) {
 	// place code for your default task here
-	console.log('Hello Gulp');
+	console.log('Hello Gulp again');
 	cb();
 }
 
-export default defaultTask;
+function printAge(cb) {
+	cb();
+	console.log('I am 41 years old');
+}
+
+const print = parallel(printName, printAge);
+
+export { print, printAge, printName };
+
+// gulp printName
+// gulp --tasks
+
+export default series(printAge, printName);
